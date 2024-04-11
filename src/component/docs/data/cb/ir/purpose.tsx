5d488fbd6df374fbe00f2f1726133f10180d1ffa
abdout <osmanabdout@hotmail.com>
2024-04-12 02:03:59 +0300

    x

--- src/component/docs/data/cb/ir/purpose.tsx
+++ src/component/docs/data/cb/ir/purpose.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Purpose = () => {
   return (
     <p>
-      The primary purpose of insulation resistance testing on circuit breakers is to assess the insulation's ability to withstand electrical stresses and maintain isolation between live conductors and ground. By measuring the insulation resistance, this testing helps to:
+      The primary purpose of insulation resistance testing on circuit breakers is to assess the insulation ability to withstand electrical stresses and maintain isolation between live conductors and ground. By measuring the insulation resistance, this testing helps to
       Identify insulation degradation, moisture ingress, or other issues that could compromise electrical safety.
       Detect potential faults or weaknesses in the insulation system before they escalate into serious electrical problems or equipment failures.
       Verify compliance with industry standards, manufacturer specifications, and regulatory requirements for electrical equipment.
